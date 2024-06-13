39afc6f7d463cec2186959ba6c78a3f6755171cd
Denis Shulyaka <Shulyaka@gmail.com>
2024-06-13 17:00:30 +0300

    Fix repo URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="voluptuous-openapi",
     version="0.0.4",
     description="Convert voluptuous schemas to OpenAPI Schema object",
-    url="http://github.com/Shulyaka/voluptuous-openapi",
+    url="https://github.com/home-assistant-libs/voluptuous-openapi",
     author="Denis Shulyaka",
     author_email="Shulyaka@gmail.com",
     license="Apache License 2.0",
