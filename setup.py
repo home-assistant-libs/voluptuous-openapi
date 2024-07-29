59b7027300aef7989cd493d2adc00ee58ac8666a
Denis Shulyaka <Shulyaka@gmail.com>
2024-07-29 13:17:36 +0300

    Version 0.0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="voluptuous-openapi",
-    version="0.0.4",
+    version="0.0.5",
     description="Convert voluptuous schemas to OpenAPI Schema object",
     url="https://github.com/home-assistant-libs/voluptuous-openapi",
     author="Denis Shulyaka",
